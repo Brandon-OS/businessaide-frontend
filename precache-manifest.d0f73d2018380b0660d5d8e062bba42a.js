ef9fa35e40c3f912a6a33628f9b352d808c41f52
Brandon-OS <hieronimusbrandon@gmail.com>
2022-07-27 17:50:52 +0800

    Updates

---
+++ precache-manifest.d0f73d2018380b0660d5d8e062bba42a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a491174e3a5503dd25dd",
+    "url": "/businessaide-frontend/static/css/main.799a790d.chunk.css"
+  },
+  {
+    "revision": "a491174e3a5503dd25dd",
+    "url": "/businessaide-frontend/static/js/main.e89341e0.chunk.js"
+  },
+  {
+    "revision": "7e041ea6991c91eb4a90",
+    "url": "/businessaide-frontend/static/js/runtime~main.39f086e2.js"
+  },
+  {
+    "revision": "8a11804c7eb74a482ba1",
+    "url": "/businessaide-frontend/static/js/2.9c9dcebf.chunk.js"
+  },
+  {
+    "revision": "743a9bf0103454f29e83",
+    "url": "/businessaide-frontend/static/js/3.85b4c877.chunk.js"
+  },
+  {
+    "revision": "0b2e08846916460ea816b1ecfa1ab37d",
+    "url": "/businessaide-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/businessaide-frontend/precache-manifest.c3f32ab78ced4fe79d85f0f36dae92e2.js"
+  "/businessaide-frontend/precache-manifest.d0f73d2018380b0660d5d8e062bba42a.js"
 );
 
 workbox.clientsClaim();
